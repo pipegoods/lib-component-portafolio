9e27ba18dce3445ab693ba015008e55d9d566fb2
andres-vizcaino <andres.vizcaino@igloo-lab.com>
2022-05-05 20:44:33 -0500

    💄 add css parser and Button

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -12,23 +15,27 @@ export default [
       {
         file: packageJson.main,
         format: "cjs",
-        sourcemap: true,
+        sourcemap: true
       },
       {
         file: packageJson.module,
         format: "esm",
-        sourcemap: true,
-      },
+        sourcemap: true
+      }
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-    ],
+      postcss(),
+      terser()
+    ]
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-  },
+    external: [/\.css$/]
+  }
 ];
